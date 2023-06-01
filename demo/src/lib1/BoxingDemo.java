bfc9050e9e8776c91dc0accc5162eee41c14cc00
srikanthpragada <srikanthpragada@gmail.com>
2023-06-01 19:56:19 +0530

    01-JUN

---
+++ demo/src/lib1/BoxingDemo.java
@@ -0,0 +1,19 @@
+package lib1;
+
+public class BoxingDemo {
+
+	public static void main(String[] args) {
+		 Object obj = "Abc";
+		 
+		 obj = 10;  // Integer 
+		 System.out.println(obj.getClass());
+		 
+		 obj = 10.50;  // Double 
+		 System.out.println(obj.getClass());
+		 
+		 Double dobj = 10.50;  // Autoboxing
+		 double d = dobj;   // Autounboxing 
+
+	}
+
+}
