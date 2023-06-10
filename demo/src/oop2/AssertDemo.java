8fe78bf07fd534acc67a1c3e2ea8dbf0ecbd4611
srikanthpragada <srikanthpragada@gmail.com>
2023-06-10 19:56:33 +0530

    10-JUN

--- demo/src/oop2/AssertDemo.java
+++ demo/src/oop2/AssertDemo.java
@@ -3,13 +3,17 @@ package oop2;
 public class AssertDemo {
 	
 	public static boolean isEven(int num) {
-		return  num / 2 == 0;
+		return  num % 2 == 0;
+	}
+	
+	public static int square(int n) {
+		return n + n;
 	}
 
 	public static void main(String[] args) {
 		 assert isEven(10) == true : "Method isEven() failed for even number";
-		 assert isEven(11) == false : "Method isEven() succeeded for odd number";
-		 assert isEven(1) ==  true : "Method isEven() failed for odd number";
+		 assert isEven(11) == false : "Method isEven() failed for odd number";
+		 assert square(10) == 100 : "square() failed";
 	}
 
 }
